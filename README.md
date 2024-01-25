eaf56516ea4826d503941547623081da674e0b2a
Ben Bartling <ben.bartling@gmail.com>
2024-01-25 12:05:19 -0600

    mega overhaul to GUI for BAC0

--- README.md
+++ README.md
@@ -1,110 +1,23 @@
-# bacnet-restapi 
+# BAC0-GUI
 
-This is a RESTful interface to retrieve data from BACnet building automation systems. The idea is to run this Flask web app on the same LAN as the building automation system along side IoT interact with BACnet protocol systems. The Flask App accepts JSON payloads in a POST request and hands off the BACnet features to BAC0 (runs on a UDP port 47808) which is a Python BACnet App developed by Christian Tremblay.
+A YABE inspired GUI APP for BAC0
 
 See BAC0 documention for what is going under the hood of the Flask App:
 https://bac0.readthedocs.io/en/latest/
 
-
-## Overview
-
-- .gitignore
-- app.py
-- README.md
-- requirements.txt
-- runtime.txt
-
-
-## Requirements
-
-See requirements.txt.
-
-## Deployment for non localhost or localhost Flask App
-See optional argument below for a `localhost` deployment
-
-## Installation & Usage
-
+## Setup
 ```bash
-$ git clone https://github.com/bbartling/bacnet-restapi.git
-
-# change the directory
-$ cd bacnet-restapi
-
-# install packages
-$ pip install -r requirements.txt
-
-# start the app
-$ python app.py
+pip install tkinter BAC0
 ```
 
-
-## Example BACnet READ request with a POST to `localhost:5000/bacnet/read` with JSON in the body:
-
-* Note: read sensor value of MSTP device address 2 on network 12345:
-```json
-{
-"address":"12345:2",
-"object_type":"analogInput",
-"object_instance":"2"
-}
-```
-returns serialized object of the BACnet present value for the point requested:
-```json
-{
-"pv": 62.75,
-"status": "success"
-}
-```
-## Example BACnet WRITE request with a POST to `localhost:5000/bacnet/write` with JSON in the body:
-* Note: Boolean objects are handled by BAC0 as strings `active` or `inactive` for `value` objects
-```json
-{
-"address":"12345:2",
-"object_type":"analogValue",
-"object_instance":"302",
-"value":"55",
-"priority":"11"
-}
-```
-returns serialized object of the BACnet present value for the point requested:
-```json
-{
-"point": "12345:2 analogValue 302 presentValue 55 - 11",
-"status": "success"
-}
-```
-## Example BACnet RELEASE request with a POST to `localhost:5000/bacnet/release` with JSON in the body:
-* Note: this statement would release analogValue 302 on BACnet priority 11
-```json
-{
-"address":"12345:2",
-"object_type":"analogValue",
-"object_instance":"302",
-"priority":"11"
-}
+## Run App
+```bash
+python app.py
 ```
 
-returns serialized object of the BACnet present value for the point requested:
-```json
-{
-"point": "12345:2 analogValue 302 presentValue null - 11",
-"status": "success"
-}
-```
+## Example
+![Alt Text](example_snip.JPG)
 
-
-* Feel free to submit a git issue or discussion for help getting started or to request a new feature!
-
-
-## Optional Args when starting the app `$ python app.py`:
-* Modify web app port number with `-port`
-* Lockout webapp outside access on rest endpoints and dashboard with `--use-localhost`
-* Create a BACnet release dashbard with `--use-dashboard`
-
-## BACnet release dashbard
-The BACnet release dashboard is a new feature (12/28/2022) that is a simple html render to give a building operator the ability to release overrides implemented through this app created from external rest API BACnet write requests. The idea is for IoT or other optimization algorithm running on the edge to interact with the buildings BACnet system through this app and if overrides are causing occupant comfort issues or other unwanted hazards a simple web dashboard is available to release BACnet overrides. More inhancements to come, stay tuned...
-
-![iot_res](/images/release_dashboard.PNG)
 
 ## Author
 
@@ -114,6 +27,6 @@ The BACnet release dashboard is a new feature (12/28/2022) that is a simple html
 
 【MIT License】
 
-Copyright 2022 Ben Bartling
+Copyright 2024 Ben Bartling
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- app.py
+++ app.py
@@ -1,201 +1,95 @@
+import tkinter as tk
+import tkinter.ttk as ttk
 import BAC0
-
-import argparse,logging,time
-from models import ReadRequestModel
-from models import WriteRequestModel
-from models import ReleaseRequestModel
-
-from flask import Flask, render_template, jsonify, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-
-from datetime import datetime
-from flask_sqlalchemy import SQLAlchemy
-from pathlib import Path
-from dataclasses import dataclass
-from flask_pydantic import validate
+import threading
+import re
 
 
-#logging.basicConfig(filename='_log_flask.log', level=logging.WARNING)
-
-#STATIC_BACNET_IP = '192.168.0.103/24'
-#bacnet = BAC0.lite(IP=STATIC_BACNET_IP)
+# Initialize BAC0
 bacnet = BAC0.lite()
 
-BASE_DIR = Path(__file__).parent
-print(BASE_DIR)
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(BASE_DIR.joinpath('db.sqlite'))
-print(SQLALCHEMY_DATABASE_URI)
+def perform_global_scan():
+    def run_scan():
+        bacnet.whois(global_broadcast=True)
+        devices = bacnet.devices
+        results = [(str(device),) for device in devices]  # Convert each device to a tuple
+        root.after(0, update_results, results)
+
+    threading.Thread(target=run_scan).start()
+
+def perform_range_scan(low_limit, high_limit):
+    def run_scan():
+        bacnet.whois(f'{low_limit} {high_limit}')
+        devices = [(dev[0], dev[1], dev[2], dev[3]) for dev in bacnet.devices]
+        root.after(0, update_results, devices)
+
+    threading.Thread(target=run_scan).start()
 
 
-db = SQLAlchemy()
+def on_double_click(event):
+    item = tree.selection()[0]
+    device_str = tree.item(item, 'values')[0]
 
-def setup_db(app):
-    app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.app = app
-    db.init_app(app)
-    print('Setup DB Success!')
-    
-    
-def db_drop_and_create_all(app):
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
-        print('DB drop all and create all Success!')
-        '''
-        example1 = BacnetOverrides(title='12345:2 analogValue 302 presentValue 55 - 11')
-        example2 = BacnetOverrides(title='12345:2 analogValue 302 presentValue 55 - 11')
-        db.session.add(example1)
-        db.session.add(example2)
-        db.session.commit()
-        BacnetOverridess = BacnetOverrides.query.all()
-        print('BacnetOverridess: ',BacnetOverridess)
-        print('example1.id: ',example1.id)
-        print('example2.id: ',example2.id)
-        print(BacnetOverrides.query.all())
-        '''
-        
+    # Use regex to split the string into parts
+    regex_pattern = r"\('(.*?)', '(.*?)', '(.*?)', (\d+)\)"
+    match = re.match(regex_pattern, device_str)
+    if match:
+        name, vendor, device_address, device_instance = match.groups()
 
-@dataclass
-class BacnetOverrides(db.Model):
-    
-    id: int
-    title: str
-    date: datetime
-    released: bool
-    
-    id = db.Column(db.Integer(), primary_key=True)
-    title = db.Column(db.String(140))
-    date = db.Column(db.DateTime(),default=datetime.now())
-    released = db.Column(db.Boolean(),default=False)
-    
-    def __init__(self, *args, **kargs):
-        super().__init__(*args, **kargs)
-        
-    def __repr__(self):
-        return f'<BacnetOverrides id: {self.id} - {self.title}'
-
-
-def make_flask_app(USE_DASHBOARD):
-    print('DASHBOARD ENABLED IS: ' + str(USE_DASHBOARD))
-
-    app = Flask(__name__)
-
-
-
-    if USE_DASHBOARD:
-        setup_db(app)
-        db_drop_and_create_all(app)
-    
-        @app.route('/')
-        def index():
-            BacnetOverridess = BacnetOverrides.query.all()
-            if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-                return jsonify(BacnetOverridess)
-            return render_template('index.html')
+        # Perform the BAC0 read operation
+        # The device_instance is already an integer thanks to the regex
+        objects = bacnet.read(f"{device_address} device {device_instance} objectList")
+        results_text.insert(tk.END, str(objects) + "\n")
     else:
-        @app.route('/')
-        def index():
-            return jsonify({'server_time': str(time.ctime())})
+        print("Invalid device format:", device_str)
 
 
-    def add_override_to_db(add_override_str):
-        print('INPUTING NEW OVERRIDE INTO DB',add_override_str)
-        do = BacnetOverrides(title=add_override_str)
-        db.session.add(do)
-        db.session.commit()
-        print('DB COMMIT SUCCESS!')
-            
+def update_results(devices):
+    tree.delete(*tree.get_children())  # Clear existing entries
+    for device in devices:
+        # Ensure the tuple has four elements, using placeholders if necessary
+        padded_device = device + ('', '') if len(device) < 4 else device
+        tree.insert('', 'end', values=padded_device)
 
-    def delete_BacnetOverrides(remove_override_str,override_id):
-        print('REMOVING OVERRIDE FROM DB',remove_override_str)
-        do = BacnetOverrides.query.filter_by(id=override_id).first()
-        db.session.delete(do)
-        db.session.commit()
-        print('DB COMMIT SUCCESS!')
+# Set up the Tkinter GUI
+root = tk.Tk()
+root.title("BACnet Scanner")
 
+# Optionally, set the initial size of the window (width x height)
+# root.geometry('800x600')  # Example: 800 pixels wide and 600 pixels tall
 
-    @app.route('/bacnet/read/', methods=['POST'])
-    @validate() # flask pydantic
-    def reader(body: ReadRequestModel):
+# Treeview for displaying BACnet devices
+tree = ttk.Treeview(root, columns=['Device Info'], show='headings')
+tree.heading('Device Info', text='Device Info')
 
-        try:
-            r = request.json 
-            read_str = f"{r['address']} {r['object_type']} {r['object_instance']} presentValue"
-            print("Excecuting read str:", read_str)
-            read_result = bacnet.read(read_str)
-            if type(float(read_result)):
-                read_result = round(read_result,2)
-            return jsonify({"status" : "success", "pv":read_result})
-        except Exception as e:
-            return jsonify({"status" : "error", "pv": e}), 500
-        
+# Set the width of the 'Device Info' column
+tree.column('Device Info', width=500)  # Example: 500 pixels wide
 
-    @app.route('/bacnet/write/', methods=['POST'])
-    @validate() # flask pydantic
-    def writer(body: WriteRequestModel):
-        
-        try:
-            r = request.json 
-            write_str = f"{r['address']} {r['object_type']} {r['object_instance']} presentValue {r['value']} - {r['priority']}"
-            print("Excecuting write str:", write_str)
-            bacnet.write(write_str)
-            if USE_DASHBOARD:
-                add_override_to_db(write_str) # add to sqlite db for dashboard
-            return jsonify({"status" : "success", "point": write_str})
-        except Exception as e:
-            return jsonify({"status" : "error", 
-                    "point": e}), 500
+tree.bind("<Double-1>", on_double_click)
+tree.pack(expand=True, fill='both')  # Allow the Treeview to expand and fill the window
 
 
-    @app.route('/bacnet/release/', methods=['POST'])
-    @validate() # flask pydantic
-    def releaser(body: ReleaseRequestModel):
-        
-        try:
-            r = request.json 
-            print("release r: ",r)
-            
-            release_str = f"{r['address']} {r['object_type']} {r['object_instance']} presentValue null - {r['priority']}"
-            print("Excecuting release str:", release_str)
-            bacnet.write(release_str)
-            if USE_DASHBOARD:
-                delete_BacnetOverrides(release_str,r['id']) # remove from sqlite db for dashboard
-            return jsonify({"status" : "success", "point": release_str})
-        except Exception as e:
-            return jsonify({"status" : "error", 
-                    "point": e}), 500
-            
-            
-    return app
+# Global scan button
+global_scan_button = tk.Button(root, text="Global BACnet Scan", command=perform_global_scan)
+global_scan_button.pack()
 
+# Range scan inputs and button
+low_limit_label = tk.Label(root, text="Low Limit")
+low_limit_label.pack()
+low_limit_entry = tk.Entry(root)
+low_limit_entry.pack()
 
-if __name__ == '__main__':
-    print("Starting main loop")
-    parser = argparse.ArgumentParser(add_help=False)
-    args = parser.add_argument_group('Options')
-    args.add_argument('-port',
-                        '--port_number',
-                        required=False,
-                        type=int,
-                        default=5000,
-                        help='Port number to run web app')    
+high_limit_label = tk.Label(root, text="High Limit")
+high_limit_label.pack()
+high_limit_entry = tk.Entry(root)
+high_limit_entry.pack()
 
-    args.add_argument('--use-localhost', default=False, action='store_true')
-    args.add_argument('--no-localhost', dest='use-localhost', action='store_false')
+range_scan_button = tk.Button(root, text="Range BACnet Scan", command=lambda: perform_range_scan(low_limit_entry.get(), high_limit_entry.get()))
+range_scan_button.pack()
 
-    args.add_argument('--use-dashboard', default=False, action='store_true')
-    args.add_argument('--no-dashboard', dest='use-dashboard', action='store_false')
-    
-    args = parser.parse_args()
-    print('args.use_dashboard: ',args.use_dashboard)
-    
-    if args.use_localhost:
-        host_address='localhost'
-        print('Running app on localhost only! App and rest endpoints will not be accessable from outside this device!')
-    else:
-        host_address='0.0.0.0'
-        
-    print('Host Address Config Is: ' + host_address)
-    app = make_flask_app(args.use_dashboard)
-    app.run(debug=False,host=host_address,port=args.port_number,use_reloader=False)
+# Text area for displaying additional information
+results_text = tk.Text(root)
+results_text.pack()
+
+# Start the GUI
+root.mainloop()
